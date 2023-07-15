58575dd6d2e476ecd6fc327aebfca4769308d7fd
MarcinC <chinek01@gmail.com>
2023-07-15 08:22:16 +0200

    some cafe file

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ Author: MC
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
 
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
@@ -46,3 +50,7 @@ class Cafe(db.Model):
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
